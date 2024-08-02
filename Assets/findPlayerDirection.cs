4c63ce60adf02dc4999585ef80b10097bd932f06
Bababooey - aka Douglas Gaddum <DG246670@falmouth.ac.uk>
2024-08-02 19:40:54 +0100

    setup for Nav work

---
+++ Assets/findPlayerDirection.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class findPlayerDirection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public LayerMask layer;
+    private Transform Player;
+    
+    void Awake()
+    {
+    Player = GameObject.Find("Player").transform;
+    }
+
+    // Update is called once per frame
+   void FixedUpdate() {
+   if(Physics.Raycast(gameObject.transform.position, Player.position, 50f, layer)){
+    gameObject.transform.GetChild(0).gameObject.SetActive(false);
+    } else {
+    gameObject.transform.GetChild(0).gameObject.SetActive(true);  
+        }
+    }
+}
