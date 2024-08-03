0803b24baa598b2d2216d22d4fa11095e69aa197
JaviWido <javiwido@hotmail.com>
2024-08-02 22:17:34 -0400

    Adding a really simple UI Options Menu

--- Assets/Javi/MenusSystem/Scripts/UI/MainMenuUI.cs
+++ Assets/Javi/MenusSystem/Scripts/UI/MainMenuUI.cs
@@ -5,13 +5,20 @@ public class MainMenuUI : MonoBehaviour {
 
 
     [SerializeField] private Button playButton;
+    [SerializeField] private Button optionsButton;
     [SerializeField] private Button quitButton;
-
+    [Space]
+    [SerializeField] private OptionsMenuUI optionsMenuUI;
 
     private void Awake() {
         playButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.GameScene);
         });
+
+        optionsButton.onClick.AddListener(() => {
+            optionsMenuUI.Show();
+        });
+
         quitButton.onClick.AddListener(() => {
             Application.Quit();
         });
